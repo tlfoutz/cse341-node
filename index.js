2fb98bbd3cba27eac50817314dbe11cb4022fb19
Travis Foutz <tfoutz@byui.edu>
2020-11-12 14:32:57 -0800

    testing/debugging prove09

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-// const express = require('express')
-// const path = require('path')
-// const PORT = process.env.PORT || 5000
-
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('pages/index'))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
 const express = require('express');
 const app = express();
-const calculateRate = require('./lib/calculateRate')
 
 app.set('port', (process.env.PORT || 5000));
 app.set('views', __dirname + '/views');
@@ -19,8 +7,11 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.render('pages/rateForm');
+//Prove 09
+const calculateRate = require('./lib/calculateRate')
+
+app.get('/prove09', function(request, response) {
+  response.render('../public/rateForm');
 });
 
 app.get('/results', calculateRate)
